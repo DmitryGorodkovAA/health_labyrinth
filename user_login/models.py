21daa90f89ad304f35823802f3a953c6662db8cf
DmitryGorodkov <dev1@nmt-9.com>
2025-04-23 14:39:14 +0400

    UserProfile

--- user_login/models.py
+++ user_login/models.py
@@ -33,3 +33,21 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    age = models.IntegerField()
+    weight = models.FloatField()
+    height = models.IntegerField()
+    gender = models.CharField(max_length=1, choices=[('M', 'Male'), ('F', 'Female')])
+
+class Forecast(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='forecast')
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class PointForecast(models.Model):
+    forecast = models.ForeignKey(Forecast, on_delete=models.CASCADE, related_name='points')
+    age = models.IntegerField()
+    percent = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
